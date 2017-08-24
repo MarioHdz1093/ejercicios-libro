751afb0d24795dbb7909a2bb462a196f05980f4e
Mario Hernandez <elziznes0@outlook.es>
2017-08-24 10:37:50 -0600

    Se agrego variables

---
+++ libro.java
@@ -0,0 +1,19 @@
+// WARNING: This file is auto-generated and any changes to it will be overwritten
+
+/**
+ * 
+ */
+public class libro
+{
+    protected String titulo;
+    protected String autor;
+
+    /**
+     * 
+     */
+    public libro(String titulo, String autor)
+    {
+        titulo = "Mario: su vida ";
+        autor = "Mario Hernandez";
+    }
+}
