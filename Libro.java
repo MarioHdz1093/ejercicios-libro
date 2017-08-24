e8ecc12fd300b4b20bcd257eaf3f78cf2023639e
Mario Hernandez <elziznes0@outlook.es>
2017-08-24 11:07:20 -0600

    terminqado

--- Libro.java
+++ Libro.java
@@ -1,20 +1,37 @@
+
 public class Libro
 {
-    protected String titulo;
-    protected String autor;
+    private String titulo;
+    private String autor;
+    private int paginas;
 
-    public Libro(String tituloLibro, String autorLibro)
+    public Libro(String tituloLibro, String autorLibro , int paginasLibro)
     {
         titulo = tituloLibro;
         autor = autorLibro;
+        paginas = paginasLibro;
     }
     
     public String dimeAutor()
     {
         return autor;
     }
+    
     public String dimeTitulo()
     {
         return titulo;
     }
+    
+    public int dimePaginas()
+    {
+        return paginas;
+    }
+    
+    public String dimeDetalles()
+    {
+        String detalles;
+        detalles = ("Titulo" +titulo+ "," + "Auto" +autor+ "," + "Paguinas" +paginas);
+        return detalles;
+    }
+    
 }
